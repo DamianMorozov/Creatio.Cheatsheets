0ea19c92f88ee2ab7a8d886b0d2eebdfe7dc95ed
Damian Viktorovich Morozov <damian@morozov33.ru>
2020-12-21 10:02:33 +0300

    Added EntitySchemaQuery.cs

---
+++ C#/EntitySchemaQuery.cs
@@ -0,0 +1,34 @@
+// ------------------------------------------------------------------------------------------------------------------------
+// EntitySchemaQuery
+// ------------------------------------------------------------------------------------------------------------------------
+var UserConnection = Get<UserConnection>("UserConnection");
+var divisionId = Get<Guid>("ScDivision");
+var nextMonthStartDate = Get<DateTime>("NextMonthStartDate");
+var parentProcessId = Get<Guid>("ParentProcessId");
+var jobGroup = "ScDistributionSalesMonthPlanningProcess";
+
+var esq = new EntitySchemaQuery(UserConnection.EntitySchemaManager, "ScRegion");
+esq.AddAllSchemaColumns();
+esq.Filters.Add(esq.CreateIsNotNullFilter("Contact"));
+esq.Filters.Add(esq.CreateFilterWithParameters(FilterComparisonType.Equal, "Division", divisionId));
+var scRegions = esq.GetEntityCollection(UserConnection);
+
+var tag = new Terrasoft.Configuration.ActivityTag(UserConnection);
+tag.SetDefColumnValues();
+tag.Name = "Заполнение плана продаж дистрибиьюции. " + Get<string>("PlanningMonth") + ". "+ Get<string>("DivisionName");
+tag.Save(false);
+Set("TagId", tag.Id);
+Set("ScRegionCount", scRegions.Count);
+
+foreach(var scRegion in scRegions)
+{
+	var parameters = new Dictionary<string, object>();
+    parameters["ScRegion"] = scRegion.PrimaryColumnValue;
+    parameters["ActivityTagId"] = tag.Id;
+    parameters["ScRegionCount"] = scRegions.Count;
+    parameters["NextMonthStartDate"] = nextMonthStartDate;
+    parameters["ParentProcessId"] = parentProcessId;
+    Terrasoft.Core.Scheduler.AppScheduler.ScheduleImmediateProcessJob(jobGroup + Guid.NewGuid().ToString(), jobGroup,
+    	"ScDistributionSalesPlanningRMProcess", UserConnection.Workspace.Name, UserConnection.CurrentUser.Name, parameters);
+}
+return true;
