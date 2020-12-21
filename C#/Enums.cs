5d49651395126f4887d8395f1b63aba652fe5e73
Damian Viktorovich Morozov <damian@morozov33.ru>
2020-12-21 10:02:41 +0300

    Added Enums.cs

---
+++ C#/Enums.cs
@@ -0,0 +1,11 @@
+// ------------------------------------------------------------------------------------------------------------------------
+// Enums
+// ------------------------------------------------------------------------------------------------------------------------
+DataValueType
+Terrasoft.Core.LookupDataValueType
+Terrasoft.Core.IntegerDataValueType
+Terrasoft.Core.MaxSizeTextDataValueType
+Terrasoft.Core.ShortTextDataValueType
+Terrasoft.Core.MediumTextDataValueType
+Terrasoft.Core.DateTimeDataValueType
+Terrasoft.Core.GuidDataValueType
