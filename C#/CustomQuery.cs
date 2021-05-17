89d6f012d20325836dd4ddb398a3dca83a7cdb05
Damian Viktorovich Morozov <damian@morozov33.ru>
2021-05-17 10:25:16 +0300

    Changed CustomQuery.cs

--- C#/CustomQuery.cs
+++ C#/CustomQuery.cs
@@ -5,43 +5,49 @@
 // ------------------------------------------------------------------------------------------------------------------------
 // Example 1.
 // ------------------------------------------------------------------------------------------------------------------------
-public void QueryExecute(UserConnection userConnection, StringBuilder queryBuilder) {
-  if (queryBuilder == null)
-    return;
-  var query = new CustomQuery(userConnection, queryBuilder.ToString());
-  query.Execute();
-  queryBuilder.Clear();
+public void QueryExecute(UserConnection userConnection, StringBuilder queryBuilder)
+{
+    if (queryBuilder == null)
+        return;
+    var query = new CustomQuery(userConnection, queryBuilder.ToString());
+    query.Execute();
+    queryBuilder.Clear();
 }
 
 // ------------------------------------------------------------------------------------------------------------------------
 // Example 2.
 // ------------------------------------------------------------------------------------------------------------------------
-public void QueryExecute(UserConnection userConnection, string query) {
-  if (queryBuilder == null)
-    return;
-  var query = new CustomQuery(userConnection, query);
-  using (var dbExecutor = userConnection.EnsureDBConnection()) {
-    using(var dr = query.ExecuteReader(dbExecutor)) {
-      DataTable dataTable;
-      while (true) {
-        var queryResult = new QueryResult();
-        dataTable = new DataTable();
-        dataTable.Load(dr);
-        queryResult.Columns = new List<string>();
-        queryResult.Columns.AddRange(dataTable.Columns.Cast<DataColumn>().Select(column => column.ColumnName));
-        if (queryResult.Columns.Count == 0) {
-          rowsAffected = (dr.RecordsAffected > 0) ? dr.RecordsAffected : 0;
-          break;
-        }
-        queryResult.Rows = new List<List<string>>();
-        foreach (DataRow row in dataTable.Rows)
+public SqlConsoleService.QueryResult QueryExecute(UserConnection userConnection, string queryString)
+{
+    if (string.IsNullOrEmpty(queryString))
+        return null;
+    var dataTable = new DataTable();
+    SqlConsoleService.QueryResult queryResult = null;
+    var query = new CustomQuery(userConnection, queryString);
+    using (var dbExecutor = userConnection.EnsureDBConnection())
+    {
+        using (var dr = query.ExecuteReader(dbExecutor))
         {
-          var r = new List<string>();
-          r.AddRange(row.ItemArray.Select(field => (field == DBNull.Value) ? "NULL" : field.ToString()));
-          queryResult.Rows.Add(r);
+            while (true)
+            {
+                queryResult = new SqlConsoleService.QueryResult();
+                dataTable.Load(dr);
+                queryResult.Columns = new List<string>();
+                queryResult.Columns.AddRange(dataTable.Columns.Cast<DataColumn>().Select(column => column.ColumnName));
+                if (queryResult.Columns.Count == 0)
+                {
+                    // rowsAffected = (dr.RecordsAffected > 0) ? dr.RecordsAffected : 0;
+                    break;
+                }
+                queryResult.Rows = new List<List<string>>();
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    var r = new List<string>();
+                    r.AddRange(row.ItemArray.Select(field => (field == DBNull.Value) ? "NULL" : field.ToString()));
+                    queryResult.Rows.Add(r);
+                }
+            }
         }
-        queryResults.Add(queryResult);
-      }
     }
-  }
+    return queryResult;
 }
